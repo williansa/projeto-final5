aa5fae840e4ed8545557073b9ca0ee3c5b9b8281
williansa <58983761+williansa@users.noreply.github.com>
2021-04-14 12:01:09 -0300

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCBm5vh6Ze30LDI-R__uc3Mx5W5wWXO9oI",
+    authDomain: "projeto5-dc59a.firebaseapp.com",
+    projectId: "projeto5-dc59a",
+    storageBucket: "projeto5-dc59a.appspot.com",
+    messagingSenderId: "611654454532",
+    appId: "1:611654454532:web:b951ba5d59493b965f1451"
+  }
+};
+ 
